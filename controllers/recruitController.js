292aea028f08bed83a924e28eadcd238bda730e0
Common_fig <gasfel95@likelion.org>
2019-08-08 23:57:53 +0900

    Update uploadRecruit

--- controllers/recruitController.js
+++ controllers/recruitController.js
@@ -1,3 +1,5 @@
+import Post from "../models/Post";
+
 export const search = (req, res) => {
     res.render("search");
 };
@@ -8,6 +10,21 @@ export const recruit = (req, res) => {
 
 export const uploadRecruit = (req, res) => {
     res.render("uploadRecruit");
+};
+
+export const postRecruit = async (req, res) => {
+    console.log(req);
+    const {
+        body: { title, playTime, location, recruitNum, description }
+    } = req;
+    const recruit = await Post.create({
+        title,
+        playTime,
+        location,
+        recruitNum,
+        description
+    });
+    recruit.save();
 };
 
 export const editRecruit = (req, res) => {
