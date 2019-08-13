7f126673150b84ac72e0bb587bf8c2aac68485db
Manngold <jsw4820@gmail.com>
2019-08-13 23:24:58 +0900

    get position to DB and marking

--- assets/js/recruitDetailMap.js
+++ assets/js/recruitDetailMap.js
@@ -1,7 +1,20 @@
 var container = document.getElementById("map");
+var latitude = document.querySelector("#latitude").value;
+var longitude = document.querySelector("#longitude").value;
+
+var parsedLat = parseFloat(latitude);
+var parsedLng = parseFloat(longitude);
+
 var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(parsedLng, parsedLat),
     level: 3
 };
-
 var map = new kakao.maps.Map(container, options);
+
+var markerPosition = new kakao.maps.LatLng(parsedLng, parsedLat);
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
